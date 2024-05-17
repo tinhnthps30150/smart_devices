53f9932cc0bfe530049971d589a7e34d11b177c1
Huy Tinh <tinhnthps30150@fpt.edu.vn>
2024-05-17 08:17:38 +0700

    Initial Commit

---
+++ smart_devices/src/main/java/com/smart_devices/SmartDevicesApplication.java
@@ -0,0 +1,13 @@
+package com.smart_devices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDevicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDevicesApplication.class, args);
+	}
+
+}
